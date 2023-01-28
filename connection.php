c4376e5f0882bf3978c000f813d87ce84dda6114
Badsha Faysal <54232503+me-badsha@users.noreply.github.com>
2023-01-28 20:36:27 +0600

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "first";  
+    $conn = new mysqli($servername, $username, $password, $db_name);
+    if($conn->connect_error){
+        die("Connection failed".$conn->connect_error);
+    }
+    echo " ";
+    
+    ?>
